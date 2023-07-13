c23f8833baa593bef587d23c81f80e7498bbb90c
cnasakhaei <69599536+cnasakhaei@users.noreply.github.com>
2023-07-13 10:21:07 +0330

    Add timer to refresh content every 30s

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,61 +1,65 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { getCoin } from '../Services/api';
-import Loader from "./Loader"
-import Coin from "./Coin"
-import styles from  './Landing.module.css'
+import Loader from './Loader';
+import Coin from './Coin';
+import styles from  './Landing.module.css';
 
 const Landing = () => {
-
-
-    const [coins, setCoins] = useState([])
-    const [search, setSearch] = useState("")
-
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
-        const fetchAPI = async () => {
-            const data = await getCoin();
-            setCoins(data)
-            
-        }
-        fetchAPI()
+        // Fetch coins initially
+        fetchCoins();
 
-    }, [])
-    
-    const searchHandler = event => {
-        setSearch(event.target.value)
-    }
+        // Refresh the page every 3 seconds
+        const interval = setInterval(() => {
+            window.location.reload();
+        }, 30000);
 
-    const searchCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+        // Cleanup the interval on unmount
+        return () => clearInterval(interval);
+    }, []);
+
+    const fetchCoins = async () => {
+        const data = await getCoin();
+        setCoins(data);
+    };
+
+    const searchHandler = (event) => {
+        setSearch(event.target.value);
+    };
+
+    const searchCoins = coins.filter(coin =>
+        coin.name.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
         <>
-            
-            {
-                coins.length ? 
-                    <div className={styles.coinContainer}>
-                        <input className={styles.input} type='text' placeholder='Search' value={search} onChange={searchHandler} />
-                {
-                    
-                            searchCoins.map(coin => <Coin
-                                key={coin.id}
-                                name={coin.name}
-                                image={coin.image}
-                                symbol={coin.symbol}
-                                price={coin.current_price}
-                                marketCap={coin.market_cap}
-                                priceChange = {coin.price_change_percentage_24h}
-                            />)
-                }
-                
-                    </div> :
-            <Loader/>
-                    
-            
-            }
-            
-        
-        
-        
+            {coins.length ? (
+                <div className={styles.coinContainer}>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        placeholder="Search"
+                        value={search}
+                        onChange={searchHandler}
+                    />
+                    {searchCoins.map(coin => (
+                        <Coin
+                            key={coin.id}
+                            name={coin.name}
+                            image={coin.image}
+                            symbol={coin.symbol}
+                            price={coin.current_price}
+                            marketCap={coin.market_cap}
+                            priceChange={coin.price_change_percentage_24h}
+                        />
+                    ))}
+                </div>
+            ) : (
+                <Loader />
+            )}
         </>
     );
 };
